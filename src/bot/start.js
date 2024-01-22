cfb6644e51e869977ec3024c1279181a6e243385
Zero Developer's <115115225+0developers@users.noreply.github.com>
2024-01-22 19:34:25 +0330

    Update start.js

--- src/bot/start.js
+++ src/bot/start.js
@@ -2,6 +2,6 @@ const { Telegraf } = require("telegraf");
 //const { Connect } = require("./proxy");
 //Connect();
 
-const bot = new Telegraf("BOTTOKEN");
+const bot = new Telegraf("6304306523:AAEuIZTfLE60cfMxiverboAhbuwt9Ty5itw");
 
 module.exports = bot;
